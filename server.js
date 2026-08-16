af5437bcbb4192773a9a62c0381ac526eb9e009e
adlerbarbosa-sudo <adler.barbosa@unimedflorianopolis.com.br>
2026-08-15 22:16:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -30,6 +30,8 @@ function readDB() { return JSON.parse(fs.readFileSync(DB_FILE, 'utf8')); }
 function writeDB(data) { fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2), 'utf8'); }
 
 let activePlayers = {}; 
+let mapHosts = {}; // O Segredo da Inteligência Artificial sem lag
+let mapEntitiesRAM = {}; 
 
 app.post('/api/register', (req, res) => {
     const { username, password } = req.body;
@@ -61,7 +63,6 @@ app.post('/api/save', (req, res) => {
     if (db.users[username]) {
         db.users[username].playerData = playerData; 
         
-        // MUDANÇA GLOBAL: Se qualquer jogador enviar atualização de mapa (dano em monstro/árvore), ele é salvo globalmente.
         if (worldData) { db.worldData = worldData; db.mapVersion = Date.now(); }
         
         if (db.users[username].role === 'admin') { 
@@ -73,7 +74,7 @@ app.post('/api/save', (req, res) => {
 });
 
 app.post('/api/sync', (req, res) => {
-    const { username, x, y, map, facing, actionAnim, equipment } = req.body;
+    const { username, x, y, map, facing, actionAnim, equipment, entities } = req.body;
     let db = readDB();
 
     if (username) { 
@@ -84,11 +85,38 @@ app.post('/api/sync', (req, res) => {
     let visiblePlayers = {};
     
     for(let u in activePlayers) {
-        if (now - activePlayers[u].lastSeen > 4000) { delete activePlayers[u]; } 
-        else if (u !== username && activePlayers[u].map === map) { visiblePlayers[u] = activePlayers[u]; }
+        if (now - activePlayers[u].lastSeen > 4000) { 
+            if (mapHosts[activePlayers[u].map] === u) {
+                delete mapHosts[activePlayers[u].map];
+            }
+            delete activePlayers[u]; 
+        } 
+        else if (u !== username && activePlayers[u].map === map) { 
+            visiblePlayers[u] = activePlayers[u]; 
+        }
     }
 
-    res.json({ players: visiblePlayers, chat: db.chat, mapVersion: db.mapVersion });
+    // Eleição de Host Inteligente: Se o Admin estiver no mapa, ele calcula. Se não, o primeiro player assume.
+    if (!mapHosts[map] || !activePlayers[mapHosts[map]] || activePlayers[mapHosts[map]].map !== map) {
+        mapHosts[map] = username;
+    } else if (username && username.toLowerCase() === 'admin') {
+        mapHosts[map] = username;
+    }
+
+    let isHost = (mapHosts[map] === username);
+
+    // O servidor só aceita posições de monstros vindas do Host oficial para não bugar
+    if (isHost && entities) {
+        mapEntitiesRAM[map] = entities;
+    }
+
+    res.json({ 
+        players: visiblePlayers, 
+        chat: db.chat, 
+        mapVersion: db.mapVersion,
+        syncEntities: mapEntitiesRAM[map] || null,
+        isHost: isHost
+    });
 });
 
 app.get('/api/map', (req, res) => { res.json({ worldData: readDB().worldData }); });
